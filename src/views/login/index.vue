47bcfaf208ad4012ed9fc4996c7395464cbb9f67
chuwenxia <5492652+chuwenxia@user.noreply.gitee.com>
2021-09-01 13:12:52 +0800

    import ant-design

---
+++ src/views/login/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="login-page">登录
+     <a-button type="primary">
+      Primary
+    </a-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  components: {},
+  props: [],
+  data () {
+    return {
+
+    }
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  methods: {}
+}
+
+</script>
+
+<style scoped lang="less">
+</style>
